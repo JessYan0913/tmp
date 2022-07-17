ad5e2de5553cb1124754d0c2638bffdf2c324e3a
JessYan <52144974+JessYan0913@users.noreply.github.com>
2022-07-17 17:26:09 +0800

    first init

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:vue/vue3-recommended', 'eslint:recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint', 'prettier'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 12,
+  },
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'es5',
+        semi: false,
+        arrowParens: 'always',
+        bracketSpacing: true,
+        printWidth: 120,
+        tabWidth: 2,
+        useTabs: false,
+        endOfLine: 'auto',
+        vueIndentScriptAndStyle: false,
+        vueIndentHTML: false,
+      },
+    ],
+  },
+}
