88575f2a511201b7d9a20c74fc2bfeda3fc25d41
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-02-10 23:15:01 +0800

    feat: 封装request工具

--- packages/utils/src/request.ts
+++ packages/utils/src/request.ts
@@ -10,7 +10,7 @@ import axios, {
 import { BaseError } from './error';
 
 export interface HttpInterceptors {
-  requestInterceptor?: (config: AxiosRequestConfig & RequestConfig) => AxiosRequestConfig | RequestConfig;
+  requestInterceptor?: (config: InternalAxiosRequestConfig) => InternalAxiosRequestConfig;
   requestInterceptorCatch?: (error: any) => any;
   responseInterceptor?: <T = AxiosResponse>(response: T) => T;
   responseInterceptorCatch?: (error: any) => any;
@@ -21,7 +21,7 @@ export interface RequestConfig extends AxiosRequestConfig, InternalAxiosRequestC
   interceptors?: HttpInterceptors;
   retry?: number;
   retryDelay?: number;
-  readonly retryCounted?: number;
+  retryCounted?: number;
 }
 
 export interface RequestError extends AxiosError<any, any>, BaseError {
@@ -34,5 +34,74 @@ export class Request {
 
   constructor(config: RequestConfig) {
     this.instance = axios.create(config);
+    this.interceptors = config.interceptors;
+
+    this.instance.interceptors.request.use(
+      this.interceptors?.requestInterceptor,
+      this.interceptors?.requestInterceptorCatch
+    );
+
+    this.instance.interceptors.response.use(
+      this.interceptors?.responseInterceptor,
+      this.interceptors?.responseInterceptorCatch
+    );
+
+    this.instance.interceptors.response.use(
+      (res: AxiosResponse) => {
+        return new Promise((resolve, reject) => {
+          if (res.status === 200) {
+            resolve(res.data);
+          } else {
+            reject(res.data);
+          }
+        });
+      },
+      async (err: RequestError) => {
+        const { code = '', config } = err;
+        if (!config || !config.retry || !['ECONNABORTED'].includes(code)) {
+          return Promise.reject(err);
+        }
+        config.retryCounted = config.retryCounted ?? 0;
+        if (config.retryCounted >= config.retry) {
+          return Promise.reject(err);
+        }
+
+        config.retryCounted++;
+        await new Promise((resolve) => {
+          setTimeout(() => {
+            resolve(undefined);
+          }, config.retryDelay);
+        });
+        return await this.request(config);
+      }
+    );
+  }
+
+  public request<T>(config: RequestConfig): Promise<T> {
+    return new Promise((resolve, reject) => {
+      try {
+        if (config.interceptors?.requestInterceptor) {
+          config = config.interceptors?.requestInterceptor(config);
+        }
+        this.instance
+          .request<any, T>(config)
+          .then((res) => {
+            if (config.interceptors?.responseInterceptor) {
+              res = config.interceptors.responseInterceptor(res);
+            }
+            resolve(res);
+          })
+          .catch((err: any) => {
+            if (config.interceptors?.responseInterceptorCatch) {
+              err = config.interceptors?.responseInterceptorCatch(err);
+            }
+            reject(err);
+          });
+      } catch (error) {
+        if (config.interceptors?.requestInterceptorCatch) {
+          config.interceptors.requestInterceptorCatch(error);
+        }
+      }
+    });
   }
 }
