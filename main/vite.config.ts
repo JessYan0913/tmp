5e58a6d682ab2cc8d32b155ddbeb564b79031e6e
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-02-19 23:10:08 +0800

    update: 更新monorepo依赖项目

--- main/vite.config.ts
+++ main/vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig(({ mode }) => {
         { find: '@', replacement: r('./src') },
         { find: /^@tmp\/utils/, replacement: join(__dirname, '../packages/utils/src/index.ts') },
         { find: /^@tmp\/h5-element/, replacement: join(__dirname, '../packages/h5-schema/src/index.ts') },
+        { find: /^@tmp\/components/, replacement: join(__dirname, '../packages/components/src/index.ts') },
         { find: 'vue', replacement: join(__dirname, './node_modules/vue/dist/vue.esm-bundler.js') },
       ],
     },
