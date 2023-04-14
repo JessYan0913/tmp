9fa9c51f1c5e17651fbc464b8cd14bcbeb4f6c68
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-04-14 20:21:12 +0800

    feat: 调试实体的移动

--- main/src/view/three/index.vue
+++ main/src/view/three/index.vue
@@ -1,16 +1,19 @@
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
 import { Context, Renderer } from '@tmp/3d-engine';
-import { BoxGeometry, Mesh, MeshBasicMaterial } from 'three';
+import { BoxGeometry, Matrix4, Mesh, MeshBasicMaterial } from 'three';
 
 const containerRef = ref<HTMLDivElement>();
 
 const context = new Context();
 const box = new BoxGeometry(5, 5, 5);
 const material = new MeshBasicMaterial({
-  color: 'aqua',
+  color: 'aquamarine',
 });
 const mesh = new Mesh(box, material);
+const translationMatrix = new Matrix4().setPosition(5, 5, 5);
+mesh.applyMatrix4(new Matrix4().multiply(translationMatrix));
+
 context.addObject(mesh);
 
 onMounted(() => {
