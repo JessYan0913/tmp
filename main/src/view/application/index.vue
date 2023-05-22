50ed824929574a5cb64889d65bcaf4c48e4808f7
Yan Heng <yanheng@siactpower.com>
2023-05-22 17:03:58 +0800

    feat: 调试useIntervalAsync Hook

--- main/src/view/application/index.vue
+++ main/src/view/application/index.vue
@@ -1,24 +1,24 @@
 <script lang="ts" setup>
-import { FileSelectCancelError, getFileExtension, IllegalFileError, selectFile } from '@tmp/utils';
+import { useIntervalAsync } from '@/hooks/useIntervalAsync';
 
-console.log(getFileExtension('ddsdsd.txt'));
-const handleSelectFile = async () => {
-  try {
-    const files = await selectFile(['.png']);
-    console.log('====', files);
-  } catch (error) {
-    if (error instanceof FileSelectCancelError) {
-      console.log('取消选择');
-    } else if (error instanceof IllegalFileError) {
-      console.log('文件格式不匹配', error.accepts);
-    }
-  }
-};
+const { flush, cancel, recover } = useIntervalAsync(async () => {
+  let timeout: any = null;
+  await new Promise((resolve) => {
+    timeout = setTimeout(() => {
+      console.log('模拟异步事件');
+      resolve('');
+    }, 2000);
+  });
+  return () => {
+    clearTimeout(timeout);
+  };
+}, 1000);
 </script>
 
 <template>
   <div>
-    <h1>应用列表</h1>
-    <button @click="handleSelectFile">选择文件</button>
+    <button @click="flush">刷新</button>
+    <button @click="cancel">取消</button>
+    <button @click="recover">恢复</button>
   </div>
 </template>
