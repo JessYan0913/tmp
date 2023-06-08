fda93107a25157d1c08ca3e33d58236508f66907
Yan Heng <yanheng@siactpower.com>
2023-06-08 10:10:28 +0800

    feat: 增加可编辑表格

--- main/src/view/page-B/index.vue
+++ main/src/view/page-B/index.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup name="Page-B">
+import { ref } from 'vue';
 import { TmpPage } from '@tmp/h5-schema';
+
+import EditTable from '@/components/EditTable.vue';
+import EditTableColumn from '@/components/EditTableColumn.vue';
 
 const config: TmpPage = {
   id: 'page-1',
@@ -42,11 +46,65 @@ const config: TmpPage = {
     },
   ],
 };
+
+const tableData = [
+  {
+    date: '2016-05-03',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-02',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-04',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+  {
+    date: '2016-05-01',
+    name: 'Tom',
+    address: 'No. 189, Grove St, Los Angeles',
+  },
+];
+
+const editTableRef = ref<InstanceType<typeof EditTable>>();
 </script>
 
 <template>
   <div>
     <h1>Page-B</h1>
     <TmpUiPage :config="config"></TmpUiPage>
+    <EditTable ref="editTableRef" :data-source="tableData">
+      <EditTableColumn prop="date" label="时间">
+        <template #edit="{ row }">
+          <input v-model="row.date" />
+        </template>
+      </EditTableColumn>
+      <EditTableColumn prop="name" label="姓名">
+        <template #edit="{ row }">
+          <input v-model="row.name" />
+        </template>
+      </EditTableColumn>
+      <EditTableColumn prop="address" label="地址">
+        <template #edit="{ row }">
+          <input v-model="row.address" />
+        </template>
+      </EditTableColumn>
+      <EditTableColumn label="操作">
+        <template #default="{ actions, $index }">
+          <button @click="actions.startEditable($index)">操作</button>
+          <button @click="actions.deleteRow($index)">删除</button>
+        </template>
+        <template #edit="{ actions, $index }">
+          <button @click="actions.saveEditable($index)">保存</button>
+          <button @click="actions.cancelEditable($index)">取消</button>
+          <button @click="actions.deleteRow($index)">删除</button>
+        </template>
+      </EditTableColumn>
+    </EditTable>
+    <button @click="editTableRef?.editActions.addRow()">新增</button>
   </div>
 </template>
