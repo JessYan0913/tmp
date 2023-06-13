7a8513e84412686a1079c21ac3d6dfc7a7d60e31
JessYan <52144974+JessYan0913@users.noreply.github.com>
2023-06-13 22:44:24 +0800

    update: editor layout

--- main/src/view/page-editor/index.vue
+++ main/src/view/page-editor/index.vue
@@ -10,57 +10,85 @@ defineComponent({
 
 <template>
   <div class="container">
-    <header class="header">Header</header>
-    <aside class="left sidebar">Sidebar</aside>
-    <main class="main">Main Content</main>
-    <aside class="right sidebar">Sidebar</aside>
-    <footer class="footer">Footer</footer>
+    <div class="tools">工具栏</div>
+    <div class="tool-panel">工具区</div>
+    <div class="vertical-divider"></div>
+    <div class="content">
+      <div class="main">
+        <div class="header">Header</div>
+        <div class="main">Main</div>
+        <div class="sidebar">Sidebar</div>
+      </div>
+      <div class="horizontal-divider"></div>
+      <div class="footer">Footer</div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
 .container {
-  display: grid;
-  grid-template-areas:
-    'header header header'
-    'sidebar-left main sidebar-right'
-    'sidebar-left footer footer';
-  grid-template-rows: auto 1fr auto;
-  grid-template-columns: auto 1fr auto;
+  display: flex;
   min-height: 100vh;
 }
+.tools {
+  width: 55px;
+  background-color: #f0f0f0;
+}
+.tool-panel {
+  width: 280px;
+  background-color: #d1d1d1;
+}
+.vertical-divider {
+  width: 1px;
+  min-height: 1vh;
+  cursor: col-resize;
 
+  &:hover {
+    width: 3px;
+    background-color: rgb(45, 45, 211);
+  }
+}
+.horizontal-divider {
+  min-width: 100%;
+  height: 1px;
+  cursor: row-resize;
+
+  &:hover {
+    height: 3px;
+    background-color: rgb(45, 45, 211);
+  }
+}
+.content {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+.main {
+  flex-grow: 1;
+  display: grid;
+  grid-template-areas:
+    'header header'
+    'main sidebar';
+  grid-template-columns: 1fr auto;
+  grid-template-rows: auto 1fr;
+}
 .header {
+  height: 85px;
   grid-area: header;
   background-color: #f0f0f0;
-  height: 50px;
-  padding: 10px;
 }
-
-.footer {
-  grid-area: footer;
-  background-color: #f0f0f0;
-  height: 100px;
-  padding: 10px;
-}
-
 .main {
   grid-area: main;
-  padding: 10px;
+  background-color: #b1b1b1;
 }
-
 .sidebar {
-  background-color: #f8f8f8;
-  padding: 10px;
+  width: 126px;
+  grid-area: sidebar;
+  background-color: #c4c4c4;
 }
-
-.left {
-  grid-area: sidebar-left;
-  width: 280px;
-}
-
-.right {
-  grid-area: sidebar-right;
-  width: 280px;
+.footer {
+  height: 280px;
+  background-color: #b1b1b1;
 }
 </style>
