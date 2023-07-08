df23ed99efcb4f13060c4ae0487d4bd521310c66
Yan Heng <yanheng@siactpower.com>
2023-07-08 16:07:53 +0800

    feat: 新增renderer项目

---
+++ renderer/vite.config.ts
@@ -0,0 +1,20 @@
+import { join } from 'path';
+
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: [
+      { find: /^@tmp\/h5-schema/, replacement: join(__dirname, '../packages/h5-schema/src/index.ts') },
+      { find: /^@tmp\/h5-core/, replacement: join(__dirname, '../packages/h5-core/src/index.ts') },
+      { find: /^@tmp\/components/, replacement: join(__dirname, '../packages/components/src/index.ts') },
+      { find: 'vue', replacement: join(__dirname, './node_modules/vue/dist/vue.esm-bundler.js') },
+    ],
+  },
+});
